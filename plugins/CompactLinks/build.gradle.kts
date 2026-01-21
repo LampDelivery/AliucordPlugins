f04b1a8ec2e7cec9e37d6c749d98c7cec55cb525
Lamp <158360149+LampDelivery@users.noreply.github.com>
2026-01-21 00:51:34 -0500

    hi

---
+++ plugins/CompactLinks/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.aliucord.plugin")
+    id("org.jetbrains.kotlin.android")
+}
+
+version = "1.0.0"
+description = "Makes links compact like DiscordRN"
+
+aliucord {
+    deploy.set(true)
+}
--- plugins/MessageLinkCompact/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-version = "1.0.0"
-description = "Makes message links compact like DiscordRN"
-
-aliucord {
-    deploy.set(true)
-}
