250d4ab8bde4ed852aa5479f948fdb8c6033581c
Lamp <158360149+LampDelivery@users.noreply.github.com>
2026-02-07 06:48:12 -0500

    feat(Cards): new plugin

---
+++ plugins/Cards/build.gradle.kts
@@ -0,0 +1,6 @@
+version = "1.0.0"
+description = "Adds cards to context menus and settings"
+
+aliucord {
+    deploy.set(true)
+}
--- plugins/DebugLogEnhancer/build.gradle.kts
+++ plugins/DebugLogEnhancer/build.gradle.kts
@@ -2,5 +2,5 @@ version = "1.0.1"
 description = "Adds search bar and filtering to debug logs"
 
 aliucord {
-    deploy.set(false)
+    deploy.set(true)
 }
